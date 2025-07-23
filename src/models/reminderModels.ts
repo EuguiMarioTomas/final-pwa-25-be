391bf159185428572ef18a512defb7fff2fd0451
tomas eugui <mariotomaseugui@gmail.com>
2025-07-23 12:26:03 -0300

    tipo y fecha agregada al modelo

--- src/models/reminderModels.ts
+++ src/models/reminderModels.ts
@@ -4,21 +4,24 @@ import { Schema, model, Document, Types} from 'mongoose';
 export interface IReminder extends Document{
   title: string;
   description: string;
-  type: 'Evento' | 'Pendiente' | 'Idea' | 'Otro';
+  type: 'Pendiente' |'Evento' | 'Idea' | 'Otro';
   createdAt: Date;
   updatedAt: Date;
   author: Types.ObjectId;
   sharedWith?: Types.ObjectId[];
   isArchived?: boolean;
+  dueDate?: Date | null;
 }
 
 const reminderSchema = new Schema<IReminder>({
   title: {type: String, required: true, trim: true},
   description: {type: String, required: true, trim: true},
-  type: {type: String, enum:['Evento', 'Pendiente', 'Idea', 'Otro'], default: 'Otro'},
+  type: {type: String, enum:['Pendiente', 'Evento', 'Idea', 'Otro'], default: 'Otro'},
   author: {type: Schema.Types.ObjectId, ref: 'User', required: true},
   sharedWith: [{type: Schema.Types.ObjectId, ref: 'User'}],
   isArchived: {type: Boolean, default: false},
+  dueDate: {type: Date, default: null},
+  
 },{
   timestamps: true,
 });
