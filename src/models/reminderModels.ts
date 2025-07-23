884c3ac9253cc1f1cbd02a43a024dc9d7cfb0322
tomas eugui <mariotomaseugui@gmail.com>
2025-07-23 17:43:01 -0300

    actualizacion de reminderSchema

--- src/models/reminderModels.ts
+++ src/models/reminderModels.ts
@@ -10,17 +10,17 @@ export interface IReminder extends Document{
   author: Types.ObjectId;
   sharedWith?: Types.ObjectId[];
   isArchived?: boolean;
-  dueDate?: Date | null;
+  dueDate?: Date;
 }
 
 const reminderSchema = new Schema<IReminder>({
   title: {type: String, required: true, trim: true},
   description: {type: String, required: true, trim: true},
-  type: {type: String, enum:['Pendiente', 'Evento', 'Idea', 'Otro'], default: 'Otro'},
+  type: {type: String, enum:['Pendiente', 'Evento', 'Idea', 'Otro'], default: 'Pendiente'},
   author: {type: Schema.Types.ObjectId, ref: 'User', required: true},
   sharedWith: [{type: Schema.Types.ObjectId, ref: 'User'}],
   isArchived: {type: Boolean, default: false},
-  dueDate: {type: Date, default: null},
+  dueDate: {type: Date},
   
 },{
   timestamps: true,
