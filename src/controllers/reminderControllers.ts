d79df941ae4928694b37b5be6f19749ad56e7c3b
tomas eugui <mariotomaseugui@gmail.com>
2025-07-23 12:24:35 -0300

    funciones get agregadas

--- src/controllers/reminderControllers.ts
+++ src/controllers/reminderControllers.ts
@@ -5,7 +5,7 @@ import User from "../models/userModels";
 //Crear recordatorio
 const createReminder = async(req: Request, res: Response) =>{
   try{
-    const {title, description, type, authorId} = req.body;
+    const {title, description, type, authorId, dueDate} = req.body;
     const author = await User.findById(authorId);
     if(!author){
       res.status(400).json({
@@ -14,7 +14,7 @@ const createReminder = async(req: Request, res: Response) =>{
       });
       return;
     }
-    const newReminder = new Reminder({title, description, type, author: authorId, sharedWith:[],});
+    const newReminder = new Reminder({title, description, type, author: authorId, sharedWith:[], dueDate,});
     await newReminder.save();
     res.status(201).json({
       message: 'Recordatorio creado exitosamente.🟢',
@@ -29,12 +29,100 @@ const createReminder = async(req: Request, res: Response) =>{
   }
 };
 
+//Obtener recordatorios por autor
+const getRemindersByAuthor = async(req:Request, res:Response)=>{
+  try{
+    const {authorId} = req.params;
+    if(!authorId){
+      res.status(400).json({
+        message: 'ID de autor es necesario para obtener recordatorios.🟡',
+        error: true,
+      });
+      return;
+    }
+    const reminders = await Reminder.find({
+      author: authorId,
+      isArchived: false, // Solo obtener recordatorios no archivados
+    }).populate('sharedWith', 'userName email').sort({type: 1});
+    res.status(200).json({
+      message: 'Recordatorios obtenidos exitosamente.🟢',
+      data: reminders,
+      error: false,
+    });
+  }catch(error:any){
+    res.status(400).json({
+      message: 'Error al obtener los recordatorios del autor.🔴',
+      error: error.message,
+    });
+  }
+}
+
+//Obtener recordatorios compartidos
+const getSharedReminders = async(req: Request, res: Response) => {
+  try{
+    const {userId} = req.params;
+    if(!userId){
+      res.status(400).json({
+        message: 'ID de usuario es necesario para obtener recordatorios compartidos.🟡',
+        error: true,
+      });
+      return;
+    }
+    const reminders = await Reminder.find({
+      sharedWith: userId,
+      isArchived: false, // Solo obtener recordatorios no archivados
+    }).populate('author', 'userName email').populate('sharedWith', 'userName email').sort({type: 1});
+    res.status(200).json({
+      message: 'Recordatorios compartidos obtenidos exitosamente.🟢',
+      data: reminders,
+      error: false,
+    });
+  }catch(error:any){
+    res.status(400).json({
+      message: 'Error al obtener los recordatorios compartidos.🔴',
+      error: error.message,
+    });
+  }
+}
+
+//obtener recordatorios archivados por autor
+const getArchivedRemindersByAuthor= async(req: Request, res: Response) => {
+  try{
+    const {authorId} = req.params;
+    if(!authorId){
+      res.status(400).json({
+        message: 'ID de autor es necesario para obtener recordatorios archivados.🟡',
+        error: true,
+      });
+      return;
+    }
+    const reminders = await Reminder.find({
+      isArchived: true,
+    }).populate('author', 'userName email').sort({type: 1});
+    res.status(200).json({
+      message: 'Recordatorios archivados obtenidos exitosamente.🟢',
+      data: reminders,
+      error: false,
+    });
+  }catch(error:any){
+    res.status(400).json({
+      message: 'Error al obtener los recordatorios archivados.🔴',
+      error: error.message,
+    });
+  }
+}
+
+//Autoactivacion de recordatorios
+// (Esta funcionalidad no está implementada en el código actual, pero se puede agregar en el futuro si es necesario)
+
+
+
 //Actualizar recordatorios
 const updateReminder = async(req: Request, res: Response)=>{
   try{
     const {id} = req.params;
-    const {title, description, type} = req.body;
-    const reminder = await Reminder.findByIdAndUpdate(id,{title, description, type}, {new: true, runValidators: true});
+    const {title, description, type, dueDate} = req.body;
+    const reminder = await Reminder.findByIdAndUpdate(id,{title, description, type, dueDate}, {new: true, runValidators: true});
     if(!reminder){
       res.status(404).json({
         message: 'Recordatorio no encontrado.🟡',
@@ -54,6 +142,7 @@ const updateReminder = async(req: Request, res: Response)=>{
   }
 };
 
+//Compartir recordatorios
 const sharedWith = async(req: Request, res: Response)=>{
   try{
     const {reminderId} = req.params;
@@ -87,6 +176,7 @@ const sharedWith = async(req: Request, res: Response)=>{
   }
 }
 
+//Archivar o restaurar recordatorios
 const softDeleteReminder = async(req: Request, res: Response) =>{
   try{
     const {reminderId} = req.params;
@@ -122,6 +212,9 @@ const softDeleteReminder = async(req: Request, res: Response) =>{
 
 export{
   createReminder,
+  getRemindersByAuthor,
+  getSharedReminders,
+  getArchivedRemindersByAuthor,
   updateReminder,
   sharedWith,
   softDeleteReminder
