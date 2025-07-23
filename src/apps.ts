09ecba0dd43bdf7316f4ec335d7955d2862d77e7
tomas eugui <mariotomaseugui@gmail.com>
2025-07-23 17:41:21 -0300

    configuracion de cors

--- src/apps.ts
+++ src/apps.ts
@@ -1,9 +1,20 @@
 import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
 
 import userRoutes from './routes/userRoutes';
 import reminderRoutes from './routes/reminderRoutes';
 
+dotenv.config();
+
 const app = express();
+
+const FRONTEND_URL = process.env.FRONTEND_URL;
+
+app.use(cors({
+  origin: FRONTEND_URL, // Cambia esto al origen de tu frontend
+  credentials: true,
+}));
 
 app.use(express.json());
 
