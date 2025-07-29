9b23fc3f77a877415abe739c6c05153fa78cb183
tomas eugui <mariotomaseugui@gmail.com>
2025-07-29 11:11:19 -0300

    refactor: agregar firebaseUid, quitar password

--- src/models/userModels.ts
+++ src/models/userModels.ts
@@ -3,17 +3,17 @@ import { Schema, model, Document } from 'mongoose';
 export interface IUser extends Document{
   userName: string;
   email: string;
-  password: string;
   createdAt: Date;
   isActive?: boolean;
+  firebaseUid: string;
 };
 
 const userSchema = new Schema<IUser>({
-  userName: { type: String, rquired: true, trim: true},
+  userName: { type: String, required: true, trim: true},
   email: { type: String, required: true, unique: true, trim: true},
-  password: { type: String, required: true, trim: true},
   createdAt: { type: Date, default:Date.now},
   isActive: { type: Boolean, default: true},
+  firebaseUid: { type: String, required: true, unique: true, trim: true}
 });
 
 const User = model<IUser>('User', userSchema);
