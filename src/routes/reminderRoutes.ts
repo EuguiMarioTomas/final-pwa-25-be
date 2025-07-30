926f30989bcbf81ff55b95a1d57d28bd6a32d720
tomas eugui <mariotomaseugui@gmail.com>
2025-07-29 22:48:28 -0300

    feat: agregar rutas recordatorio obtener por id, a punto de vencer, editar

--- src/routes/reminderRoutes.ts
+++ src/routes/reminderRoutes.ts
@@ -5,6 +5,8 @@ import {
   getRemindersByAuthor,
   getSharedReminders,
   getArchivedRemindersByAuthor,
+  getReminderById,
+  getUpcomingReminders,
   updateReminder,
   sharedWith,
   softDeleteReminder,
@@ -16,7 +18,9 @@ router.post('/createReminder', createReminder);
 router.get('/getRemindersByAuthor/:authorId', getRemindersByAuthor);
 router.get('/getSharedReminders/:userId', getSharedReminders);
 router.get('/getArchivedRemindersByAuthor/:authorId', getArchivedRemindersByAuthor);
-router.put('/updateReminder/:id', updateReminder);
+router.get('/getReminderById/:reminderId', getReminderById);
+router.get('/getUpcomingReminders/:authorId', getUpcomingReminders);
+router.put('/updateReminder/:reminderId', updateReminder);
 router.patch('/sharedReminder/:reminderId', sharedWith);
 router.patch('/softDeleteReminder/:reminderId', softDeleteReminder);
 
